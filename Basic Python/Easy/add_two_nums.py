4e12f4d76bb486b9d34a6132278d04adeb6808a3
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2025-05-14 04:44:52 -0700

    Initial Commit

---
+++ Basic Python/Easy/add_two_nums.py
@@ -0,0 +1,10 @@
+"""
+Add Two Numbers
+Take two numbers from the user and print their sum.
+"""
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+addition = a + b
+print(f"The sum of {a} + {b} = {addition}")
